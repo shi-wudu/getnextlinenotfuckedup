bac8fa2f491ff30eb2bbf950c7651f329081858c
Marta Leandro Faria Romao De Figueiredo <marleand@c1r8s10.42lisboa.com>
2025-01-04 17:22:44 +0000

    meow

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marleand <marleand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 16:55:20 by marleand          #+#    #+#             */
-/*   Updated: 2025/01/04 17:21:02 by marleand         ###   ########.fr       */
+/*   Updated: 2025/01/04 17:22:23 by marleand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*get_next_line(int fd)
 	int			check;
 
 	line = NULL;
-	if (fd < 0 && fd > FOPEN_MAX || BUFFER_SIZE < 1)
+	if ((fd < 0 && fd > FOPEN_MAX) || BUFFER_SIZE < 1)
 		return (line);
 	check = read(fd, buffer, BUFFER_SIZE);
 	while (*buffer || check > 0)
@@ -34,7 +34,7 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/* # include <sys/types.h>
+# include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 
@@ -57,4 +57,4 @@ int	main(void)
 	printf("BUFFER SIZE: %i\n", BUFFER_SIZE);
 	close(fd);
 	return (0);
-} */
+}
